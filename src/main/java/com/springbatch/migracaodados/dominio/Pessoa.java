19d5e2bab266d6fdc9e5a06e615144da4f4c005e
Danilo Azevedo <danilo.ann@gmail.com>
2023-04-29 15:55:03 -0300

    Inserido verificação de pessoas Inváldas e arquivo de teste

--- src/main/java/com/springbatch/migracaodados/dominio/Pessoa.java
+++ src/main/java/com/springbatch/migracaodados/dominio/Pessoa.java
@@ -1,5 +1,7 @@
 package com.springbatch.migracaodados.dominio;
 
+import org.apache.logging.log4j.util.Strings;
+
 import java.util.Date;
 
 public class Pessoa {
@@ -48,5 +50,9 @@ public class Pessoa {
 
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public boolean isValida() {
+        return !Strings.isBlank(nome) && !Strings.isBlank(email) && dataNascimento != null;
     }
 }
